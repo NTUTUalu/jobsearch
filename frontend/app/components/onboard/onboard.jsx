03f97aea37dc19e63a73e4b2d55fd3a779b04f34
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-04-23 13:05:23 +0200

    added a submit button

--- frontend/app/components/onboard/onboard.jsx
+++ frontend/app/components/onboard/onboard.jsx
@@ -1,6 +1,7 @@
-"use client";
 import React from "react";
 import { BackgroundBeams } from "./background-beam";
+import "./onboard.module.css"; 
+import Styles from "./onboard.module.css"; // Correct import path
 
 export default function Onboard() {
   return (
@@ -9,15 +10,32 @@ export default function Onboard() {
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Join the waitlist
         </h1>
-        <p></p>
+    
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-        Join Us Early! Get Updates on Our Launch!
+          Join Us Early! Get Updates on Our Launch!
         </p>
+        <div className="flex w-full justify-center items-center bg-pink-1 mt-4">
         <input
           type="text"
+          maxLength="45"
           placeholder="hi@manuarora.in"
-          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4 p-2 bg-neutral-950 placeholder:text-neutral-700"
+          className="rounded-lg border text-white font-light border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10  p-2 bg-neutral-950 placeholder:text-neutral-700"
         />
+     
+     <button className={`${Styles.btn} ml-4 `} type="button">
+  <strong className={`${Styles.strong}  `}>JOIN</strong>
+  <div id="container-stars">
+    <div id="stars"></div>
+  </div>
+
+  <div id="glow">
+    <div className={`${Styles.circle}  `}></div>
+    <div className={`${Styles.circle}  `}></div>
+  </div>
+</button>
+        </div>
+       
+
       </div>
       <BackgroundBeams />
     </div>
