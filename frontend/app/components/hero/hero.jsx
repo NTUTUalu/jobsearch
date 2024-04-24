a33e6c3f089d11a46de888043d1186d9d706b763
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-04-24 18:24:04 +0200

    modified the ui for small screens

--- frontend/app/components/hero/hero.jsx
+++ frontend/app/components/hero/hero.jsx
@@ -6,7 +6,7 @@ import Styles from "./hero.module.css";
 export default function MoodSetter() {
   return (
     <>
-      <div className="relative h-screen">
+      <div className="relative h-screen max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
         {/* Video background */}
         <video
           className="absolute top-0 left-0 w-full h-full object-cover -z-10"
@@ -28,13 +28,14 @@ export default function MoodSetter() {
         <div className={`${Styles.gradient} absolute top-0 left-0 w-full h-full bg-black opacity-60 `}></div>
         
 
-        <div className="px-20  max-md:px-10 max-sm:px-1 max-sm:p wrapper flex w-full h-screen max-md:h-fit max-sm:flex-col">
-          <div className="left flex flex-col w-1/2 mt-24 max-md:mt-12 max-sm:w-full max-sm:mt-0 max-sm:items-center">
+        <div className="px-20  max-md:px-10 max-sm:px-1 max-sm:p wrapper flex w-full h-screen max-md:h-fit max-sm:flex-col  max-sm:h-4/6 max-sm:justify-center max-sm:bg-black max-sm:bg-opacity-20">
+          <div className="left flex flex-col w-1/2 mt-24 max-md:mt-12 max-sm:w-full max-sm:mt-0 max-sm:items-center
+          max-sm:justify-evenly  max-sm:h-5/6">
             <h1 className="text-5xl text-slate-300 font-bold w-5/6 leading-tight max-md:text-3xl max-md:w-5/6 max-sm:text-center max-sm:w-full max-sm:text-2xl max-sm:mt-4 ">
               Tired of  <span className="text-amber-500  max-md:text-" >job  <br />
               search </span><span className="text-slate-300 max-md:text-">frustrations?</span>
             </h1>
-            <p className="text-slate-400 font-normal w-6/6 mt-8 mb-7 max-md:w-5/6 max-sm:w-full max-sm:px-2 max-sm:tracking-wider">
+            <p className="text-slate-400 font-normal w-6/6 mt-8 mb-7 max-md:w-5/6 max-sm:w-full max-sm:px-2 max-sm:text-base max-sm:tracking-wider ">
               Are you a graduate or student in Lesotho searching for job placements? Join the waitlist for Lesotho's first-ever job search platform! We're here to help you find the perfect opportunity to kickstart your career journey. Don't miss out – sign up now!
             </p>
             <div className={` flex`}>
@@ -45,7 +46,7 @@ export default function MoodSetter() {
               </Link>
             </div>
           </div>
-          <div className="right w-1/2 max-sm:order-first max-sm:w-full max-sm:flex max-sm:justify-center">
+          <div className="right w-1/2 max-sm:w-full max-sm:justify-center  max-sm:hidden">
             
           </div>
         </div>
--- frontend/app/components/onboard/onboard.jsx
+++ frontend/app/components/onboard/onboard.jsx
@@ -58,11 +58,15 @@ export default function Onboard() {
 
   return (
     <div
-      className="h-[40rem] w-full  bg-neutral-950 relative flex flex-col items-center justify-center m-0"
+      className="h-[40rem] w-full  bg-neutral-950 relative flex flex-col items-center justify-center m-0 max-sm:"
       id="onboard"
     >
       <Toaster position="top-center" reverseOrder={false} />
-      <div className="max-w-2xl mx-auto p-4">
+      <div className="max-w-2xl mx-auto p-4 max-sm:flex-col max-sm:flex">
+      
+      <div className="hidden max-sm:flex max-sm:w-full max-sm:bg-pink-5 max-sm:justify-center max-sm:mb-4  ">
+      <img src="/on.svg" alt="Description" className="w-36 h-36"/>
+      </div>
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Join the waitlist
         </h1>
@@ -70,7 +74,7 @@ export default function Onboard() {
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
           Join Us Early! Get Updates on Our Launch!
         </p>
-        <form className="flex w-full justify-center items-center bg-pink-1 mt-4" autoComplete="off"
+        <form className="flex w-full justify-center items-center bg-pink-1 mt-4 max-sm:flex-col max-sm:flex max-sm:h-24 max-sm:justify-between bg-pink" autoComplete="off"
                 onSubmit={handleSubmit}>
           <input
             type="email"
@@ -82,7 +86,7 @@ export default function Onboard() {
             className="rounded-lg border text-white font-extralight border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 tracking-wider p-2 bg-neutral-950 placeholder:text-neutral-700 text-sm"
           />
 
-          <button className={`${Styles.btn} ml-4  `}  type="submit">
+          <button className={`${Styles.btn} ml-4  max-sm:w-full  max-sm:m-0 max-sm: `}  type="submit">
             <strong className={`${Styles.strong}  `}>JOIN</strong>
             <div id="container-stars">
               <div id="stars"></div>
