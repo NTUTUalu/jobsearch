ef7efa18dd6f2fdf98937a12c4b05870855f1a4f
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-04-24 19:10:55 +0200

    modified the ui

--- frontend/app/components/hero/hero.jsx
+++ frontend/app/components/hero/hero.jsx
@@ -29,13 +29,13 @@ export default function MoodSetter() {
         
 
         <div className="px-20  max-md:px-10 max-sm:px-1 max-sm:p wrapper flex w-full h-screen max-md:h-fit max-sm:flex-col  max-sm:h-4/6 max-sm:justify-center max-sm:bg-black max-sm:bg-opacity-20">
-          <div className="left flex flex-col w-1/2 mt-24 max-md:mt-12 max-sm:w-full max-sm:mt-0 max-sm:items-center
+          <div className="left flex flex-col w-1/2 mt-24 max-md:mt-12 max-md:w-10/12 max-sm:w-full max-sm:mt-0 max-sm:items-center
           max-sm:justify-evenly  max-sm:h-5/6">
             <h1 className="text-5xl text-slate-300 font-bold w-5/6 leading-tight max-md:text-3xl max-md:w-5/6 max-sm:text-center max-sm:w-full max-sm:text-2xl max-sm:mt-4 ">
               Tired of  <span className="text-amber-500  max-md:text-" >job  <br />
               search </span><span className="text-slate-300 max-md:text-">frustrations?</span>
             </h1>
-            <p className="text-slate-400 font-normal w-6/6 mt-8 mb-7 max-md:w-5/6 max-sm:w-full max-sm:px-2 max-sm:text-base max-sm:tracking-wider ">
+            <p className="text-slate-400 font-normal w-6/6 mt-8 mb-7 max-md:w-6/6 max-sm:w-full max-sm:px-2 max-sm:text-base max-sm:tracking-wider ">
               Are you a graduate or student in Lesotho searching for job placements? Join the waitlist for Lesotho's first-ever job search platform! We're here to help you find the perfect opportunity to kickstart your career journey. Don't miss out – sign up now!
             </p>
             <div className={` flex`}>
@@ -46,7 +46,7 @@ export default function MoodSetter() {
               </Link>
             </div>
           </div>
-          <div className="right w-1/2 max-sm:w-full max-sm:justify-center  max-sm:hidden">
+          <div className="right w-1/2 max-sm:w-full max-sm:justify-center  max-md:hidden max-sm:hidden">
             
           </div>
         </div>
--- frontend/app/components/onboard/onboard.jsx
+++ frontend/app/components/onboard/onboard.jsx
@@ -64,10 +64,10 @@ export default function Onboard() {
       <Toaster position="top-center" reverseOrder={false} />
       <div className="max-w-2xl mx-auto p-4 max-sm:flex-col max-sm:flex">
       
-      <div className="hidden max-sm:flex max-sm:w-full max-sm:bg-pink-5 max-sm:justify-center max-sm:mb-4  ">
+      <div className="hidden max-md:flex max-sm:w-full max-sm:bg-pink-5 max-md:justify-center max-sm:mb-4  ">
       <img src="/on.svg" alt="Description" className="w-36 h-36"/>
       </div>
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold max-sm:text-2xl">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold max-sm:text-2xl max-md:text-4xl">
           Join the waitlist
         </h1>
 
